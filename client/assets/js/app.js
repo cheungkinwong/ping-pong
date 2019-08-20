a94814bc0ad56fda55dd7de9391e2248635eb57b
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-20 12:25:21 +0200

    installed socket io on client and server

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -310,3 +310,8 @@ function collision(circle, player) {
 }
 
 //------------------------------------------------socket io
+var socket = io("http://localhost");
+socket.on("news", function(data) {
+     console.log(data);
+     socket.emit("my other event", { my: "data" });
+});
